c6929030da5988411d46c24a5256eb8dd3ba22a7
우태호 <{>
2025-11-03 15:13:05 +0900

    [INIT] spring 초기 세팅

---
+++ src/test/java/com/gifpt/GifptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gifpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GifptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
